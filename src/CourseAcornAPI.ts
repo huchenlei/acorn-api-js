c2391e9d4cc31333dc540b792b335bc8cc8b859f
CharlieUofT <chenlei.hu@mail.utoronto.ca>
2017-10-04 20:39:55 -0400

    implement enroll/getExtraCourseInfo and their test-case in CourseAcornAPI

--- src/CourseAcornAPI.ts
+++ src/CourseAcornAPI.ts
@@ -5,6 +5,7 @@ import rp = require('request-promise');
 import querystring = require('querystring');
 import {AcornError} from "./AcornError";
 import _ = require('lodash');
+import assert = require('assert');
 
 /**
  * This class handles all course related actions on acorn
@@ -29,6 +30,12 @@ function needRegistration(target: any, propertyKey: string, descriptor: any) {
         return originalMethod.apply(this, args);
     };
     return descriptor;
+}
+
+interface CookieValue {
+    key: string;
+    value: string;
+    [key: string]: any;
 }
 
 export class CourseAcornAPI extends BaseAcornAPI {
@@ -77,6 +84,89 @@ export class CourseAcornAPI extends BaseAcornAPI {
             ['candidacyPostCode', 'candidacySessionCode', 'sessionCode']));
         return await rp.get({
             uri: 'https://acorn.utoronto.ca/sws/rest/enrolment/plan?' + getQueryStr,
+            jar: this.state.cookieJar,
+            json: true
+        });
+    }
+
+    /**
+     * Normally registrationIndex = 1 is summer course
+     * {"course":{"code":"CSC236H1","sectionCode":"Y","primaryTeachMethod":"LEC","enroled":false},"lecture":{"sectionNo":"LEC,5101"},"tutorial":{},"practical":{}}
+     *
+     * [WARNING] this method has not been tested yet;
+     * Currently its logic is directly copied from Acorn API(Java)
+     * @param registrationIndex
+     * @param code courseCode
+     * @param sectionCode
+     * @param lectureSectionNo
+     */
+    @needLogin
+    @needRegistration
+    public async enroll(registrationIndex: number,
+                        code: string,
+                        sectionCode: string,
+                        lectureSectionNo: string): Promise<boolean> {
+        const payload = {
+            activeCourse: {
+                course: {
+                    code,
+                    sectionCode: sectionCode.toUpperCase(),
+                    primaryTeachMethod: "LEC",
+                    enrolled: "false"
+                },
+                lecture: {
+                    sectionNo: lectureSectionNo.toUpperCase()
+                },
+                tutorial: {},
+                practical: {}
+            },
+            eligRegParams: this.cachedRegistrations[registrationIndex].registrationParams
+        };
+        let xsrfToken = "";
+        this.state.cookieJar.getCookies('https://acorn.utoronto.ca').forEach(cookie => {
+            const cv = <CookieValue>JSON.parse(JSON.stringify(cookie));
+            if (cv.key === 'XSRF-TOKEN') {
+                xsrfToken = cv.value;
+            }
+        });
+
+        assert(xsrfToken !== "", "unable to locate xsrf-token in cookies");
+        const res = await rp.post({
+            uri: 'https://acorn.utoronto.ca/sws/rest/enrolment/course/modify',
+            body: payload,
+            headers: {
+                "X-XSRF-TOKEN": xsrfToken
+            }
+        });
+        return true;
+    }
+
+    /**
+     * This method loads some extra information on courses
+     * @param registrationIndex
+     * @param courseCode
+     * @param courseSessionCode
+     * @param sectionCode
+     * @return {TRequest}
+     */
+    @needLogin
+    @needRegistration
+    public async getExtraCourseInfo(registrationIndex: number,
+                                    courseCode: string,
+                                    courseSessionCode: string,
+                                    sectionCode: string): Promise<Acorn.Course> {
+        const getQueryStr =
+            querystring.stringify(this.cachedRegistrations[registrationIndex].registrationParams) + '&' +
+            querystring.stringify({
+                activityApprovedInd: "",
+                activityApprovedOrg: "",
+                courseCode,
+                courseSessionCode,
+                sectionCode
+            });
+
+        return await rp.get({
+            uri: 'https://acorn.utoronto.ca/sws/rest/enrolment/course/view?' + getQueryStr,
             jar: this.state.cookieJar,
             json: true
         });
--- test/CourseAcornAPI.spec.ts
+++ test/CourseAcornAPI.spec.ts
@@ -5,8 +5,10 @@ import {expect} from "chai";
 import {CourseAcornAPI} from "../src/CourseAcornAPI";
 import {BasicAcornAPI} from "../src/BasicAcornAPI";
 import {AcornStateManager} from "../src/AcornAPI";
+import {AcornError} from '../src/AcornError';
 import request = require("request");
-
+import assert = require('assert');
+import _ = require("lodash");
 
 
 require('chai').use(require('chai-as-promised')).should();
@@ -45,12 +47,36 @@ describe('CourseAcornAPI', async function () {
         (<Array<any>>res)[0].should.haveOwnProperty('registrationParams');
     });
 
-    it('should get enrolled courses if logged in', async function() {
+    it('should get enrolled courses if logged in', async function () {
         await courseAPI.getEnrolledCourses();
     });
 
-    it('should get carted courses if logged in', async function() {
-        let res = await courseAPI.getCartedCourses();
-        require('fs').writeFileSync('./cart.json', JSON.stringify(res));
+    let cartedCourse: Acorn.CartedCourse | null = null;
+    it('should get carted courses if logged in', async function () {
+        const cartedCourses = await courseAPI.getCartedCourses();
+        // For later testing use
+        if (cartedCourses.length > 0) {
+            cartedCourse = cartedCourses[0];
+        }
+    });
+
+    // TODO test when course registration is open
+    // it('should enroll course if logged in', async function () {
+    //     await courseAPI.enroll(0, "CSC467", "F", "LEC,0101");
+    // });
+
+    /**
+     * To test this part
+     * The tester's acorn account must have at least one course in enrollment cart
+     */
+    it('should get extra course info if logged in', async function () {
+        if (cartedCourse === null) {
+            throw new AcornError('No course in course cart, unable to find extra info');
+        }
+        const sessionCodes = _.compact(_.values<string>(_.pick(cartedCourse,
+            ['regSessionCode1', 'regSessionCode2', 'regSessionCode3'])));
+        assert(sessionCodes.length > 0, 'no session code available'); // need at least one sessionCode
+        const res = await courseAPI.getExtraCourseInfo(
+            0, cartedCourse.courseCode, sessionCodes[0], cartedCourse.sectionCode);
     });
 });
